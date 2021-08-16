80f12c99166074919dc2d39c7a345769d56a2138
hello <test@test.com>
2021-08-16 13:40:56 -0400

    Add a little more clarity to what's going on upon failure; move destination outside of SecLists to not clutter it up

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
+#!/bin/bash
 DUPLICUTDIR=~/opt/duplicut
 HATECRACKDIR=/opt/hatecrack
 WORDLISTTEMPDIR=~/_temphatecrackwordlists
-WORDLISTFINALDIR=/opt/SecLists/Passwords/packed_wordlists
-WORDLISTPACKEDFILE=$WORDLISTFINALDIR/seclists_packed.txt
+WORDLISTFINALDIR=/opt/seclists-ordered
+WORDLISTPACKEDFILE=$WORDLISTFINALDIR/seclists-ordered.txt
 
 echo 'Checking that all the files exist...'
-if ! (./checkfiles.sh ordered-wordlists.txt); then echo 'Not all files found, exiting!'; exit 1; fi
+if ! (./checkfiles.sh ordered-wordlists.txt); then
+	echo 'Not all Seclist files in ordered-wordlists.txt found. Before running this script you must install the SecLists, extract rockyou.txt from its archive,'
+	echo 'and make sure the locations of the files on disk match those of ordered-wordlists.txt'
+	echo 'e.g.'
+	echo 'git clone https://github.com/danielmiessler/SecLists /opt/SecLists'
+	echo 'tar -xvf /opt/SecLists/Passwords/Leaked-Databases/rockyou.txt.tar.gz -C /opt/SecLists/Passwords/Leaked-Databases'
+	exit 1;
+fi
 
 echo 'Installing duplicut... (it might be already installed, just ignore the errors in that case!)'
 git clone https://github.com/nil0x42/duplicut $DUPLICUTDIR && cd $DUPLICUTDIR && make
@@ -23,10 +31,12 @@ echo 'Duplicutting the temp file...'
 mkdir -p $WORDLISTFINALDIR
 $DUPLICUTDIR/duplicut $WORDLISTTEMPDIR/seclists-catted.txt -o $WORDLISTPACKEDFILE
 
-echo "Here are the number of lines in $WORDLISTPACKEDFILE"
+echo "Here are the number of lines in $WORDLISTPACKEDFILE (should not be zero)"
 wc -l $WORDLISTPACKEDFILE
 
-echo "Don't forget to remove the temp wordlist $WORDLISTTEMPDIR/seclists-catted.txt"
-echo "rm $WORDLISTTEMPDIR/seclists-catted.txt"
+echo "Removing wordlist $WORDLISTTEMPDIR/seclists-catted.txt ..."
+rm $WORDLISTTEMPDIR/seclists-catted.txt
+
+echo "Finished"
 
 
